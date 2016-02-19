4b1540ef962b39f1a818e4b794c9c2abf6f943ae
skatardude10 <skatardude10@gmail.com>
2016-02-18 22:30:30 -0800

    Automatic Update

--- push-files.sh
+++ push-files.sh
@@ -1,1 +1,1 @@
-cp ~/dotfiles/i3/config ~/.config/i3/ && cp ~/dotfiles/.xinitrc ~/ && cp ~/dotfiles/.Xresources ~/ && cp ~/dotfiles/.yaourtrc ~/ && cp ~/dotfiles/.bash_profile ~/ && cp ~/dotfiles/.bashrc ~/; xrdb ~/.Xresources
+cp ~/dotfiles/i3/config ~/.config/i3/ && cp ~/dotfiles/.xinitrc ~/ && cp ~/dotfiles/.Xresources ~/ && cp ~/dotfiles/.yaourtrc ~/ && cp ~/dotfiles/.bash_profile ~/ && cp ~/dotfiles/.bashrc ~/ && cp ~/dotfiles/ncmpcpp/config ~/.ncmpcpp/; xrdb ~/.Xresources
