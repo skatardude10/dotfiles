b0b4a73130e9253c26ce86f02e109377f9864a37
skatardude10 <skatardude10@gmail.com>
2016-02-29 20:37:34 -0800

    Automatic Update

--- drop-term.sh
+++ drop-term.sh
@@ -1,9 +1,9 @@
 ### Restart Drop Down ###
-sleep 2
+sleep 30
 while true; do
 	if ps aux | grep '[u]rxvt -name dropterm' 1>&1
 		then
-			sleep 15
+			sleep 30
 		else
 			urxvt -name dropterm -geometry 150x20
 	fi
