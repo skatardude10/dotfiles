afa3c4861b9488ab038570ec0ab3a276fa595336
skatardude10 <skatardude10@gmail.com>
2016-03-21 20:52:05 -0700

    Automatic Update

--- wallpaper-script.sh
+++ wallpaper-script.sh
@@ -15,7 +15,15 @@ then
 else
 	if [[ $(ls /sys/class/net) =~ .*"wlp3s0".* ]] 
 	then
-		feh --bg-fill ~/dotfiles/wallpapers/wallpaper.png # "${files[RANDOM % range]}" "${files[RANDOM % range]}"
+		while true; do
+			files=()
+			for i in *.jpg *.png; do
+				[[ -f $i ]] && files+=("$i")
+			done
+			range=${#files[@]}
+			((range)) && feh --bg-fill "${files[RANDOM % range]}" # "${files[RANDOM % range]}" "${files[RANDOM % range]}"
+			sleep 10m
+		done
 	else
 		if [[ $(ls /sys/class/net) =~ .*"enp4s0".* ]] 
 		then
