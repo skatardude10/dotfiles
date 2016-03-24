ff0c929893ffadc1614e0053d02095401bec8dd2
skatardude10 <skatardude10@gmail.com>
2016-03-23 20:53:52 -0700

    Automatic Update

--- wallpaper-script.sh
+++ wallpaper-script.sh
@@ -1,41 +1,12 @@
 #!/bin/bash
 shopt -s nullglob
 cd ~/dotfiles/wallpapers
-if [[ $(ls /sys/class/net) =~ .*"wlp0s26u1u1".* ]] 
-then
-	while true; do
-		files=()
-		for i in *.jpg *.png; do
-			[[ -f $i ]] && files+=("$i")
-		done
-		range=${#files[@]}
-		((range)) && feh --bg-fill "${files[RANDOM % range]}" # "${files[RANDOM % range]}" "${files[RANDOM % range]}"
-		sleep 10m
+while true; do
+	files=()
+	for i in *.jpg *.png; do
+		[[ -f $i ]] && files+=("$i")
 	done
-else
-	if [[ $(ls /sys/class/net) =~ .*"wlp3s0".* ]] 
-	then
-		while true; do
-			files=()
-			for i in *.jpg *.png; do
-				[[ -f $i ]] && files+=("$i")
-			done
-			range=${#files[@]}
-			((range)) && feh --bg-fill "${files[RANDOM % range]}" # "${files[RANDOM % range]}" "${files[RANDOM % range]}"
-			sleep 10m
-		done
-	else
-		if [[ $(ls /sys/class/net) =~ .*"enp4s0".* ]] 
-		then
-			while true; do
-				files=()
-				for i in *.jpg *.png; do
-					[[ -f $i ]] && files+=("$i")
-				done
-				range=${#files[@]}
-				((range)) && feh --bg-fill "${files[RANDOM % range]}" # "${files[RANDOM % range]}" "${files[RANDOM % range]}"
-				sleep 10m
-			done
-		fi
-	fi
-fi
+	range=${#files[@]}
+	((range)) && feh --bg-fill "${files[RANDOM % range]}" # "${files[RANDOM % range]}" "${files[RANDOM % range]}"
+	sleep 10m
+done
