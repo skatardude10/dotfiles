590d91a8540d02c9ed601c9c536455a861a7f3ac
skatardude10 <skatardude10@gmail.com>
2016-05-08 13:47:23 -0600

    Automatic Update

---
+++ power.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+res=$(echo "logout|reboot|shutdown" | rofi -sep "|" -dmenu -i -p 'Power Menu: ' "" -width 9 -hide-scrollbar -location 5 -padding 15 -sidebar-mode -font "Ubuntu Condensed 16" -auto-select)
+
+if [ $res = "logout" ]; then
+    i3-msg exit
+fi
+if [ $res = "reboot" ]; then
+    systemctl reboot
+fi
+if [ $res = "shutdown" ]; then
+    systemctl poweroff
+fi
+exit 0
