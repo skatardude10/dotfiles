9e037d48aee03c98ea30c1ff92fc2ad978653024
Dane Carder <skatardude10@gmail.com>
2016-05-29 02:02:35 -0600

    Automatic Update

---
+++ scripts/apply2dunst.sh
@@ -0,0 +1,11 @@
+
+cat ~/.Xresources | grep "define FNT " | cut -c 13- > ~/.config/dunst/dunstvars
+cat ~/.Xresources | grep "define SIZ " | cut -c 13- >> ~/.config/dunst/dunstvars
+cat ~/.Xresources | grep "define x" | tr -d " " | cut -c 16- | tr -d "#" | sed "s/und//" >> ~/.config/dunst/dunstvars
+mapfile -t dunstarray <~/.config/dunst/dunstvars
+sed -i 's/.* font = .*/    font = '"${dunstarray[0]}"' '"${dunstarray[1]}"'/' ~/.config/dunst/dunstrc
+sed -i 's/.* color = .*/     color = "#'"${dunstarray[2]}"'"/' ~/.config/dunst/dunstrc
+sed -i 's/.*background.*/    background = "#'"${dunstarray[2]}"'"/' ~/.config/dunst/dunstrc
+sed -i 's/.*foreground.*/    foreground = "#'"${dunstarray[7]}"'"/' ~/.config/dunst/dunstrc
+pkill dunst
+notify-send 'synchronization complete'
