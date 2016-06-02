7be2191012148445194eb9fe9cbeb5e5f62da3fb
Dane Carder <skatardude10@gmail.com>
2016-06-02 10:36:52 -0600

    Automatic Update

--- scripts/power.sh
+++ scripts/power.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-res=$(echo "logout|reboot|shutdown" | rofi -sep "|" -dmenu -i -p 'Power: ' "" -hide-scrollbar -width 10 -location 5 -sidebar-mode)
+res=$(echo "logout|reboot|suspend|shutdown" | rofi -sep "|" -dmenu -i -p 'Power: ' "" -hide-scrollbar -width 10 -location 5 -sidebar-mode)
 
 if [ $res = "logout" ]; then
     i3-msg exit
 fi
 if [ $res = "reboot" ]; then
     systemctl reboot
+fi
+if [ $res = "suspend" ]; then
+    systemctl suspend
 fi
 if [ $res = "shutdown" ]; then
     systemctl poweroff
