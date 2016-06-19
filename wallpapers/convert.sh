1f28ae901cccb4a27c31f48a5147889332ee4cec
Dane Carder <skatardude10@gmail.com>
2016-06-18 23:27:05 -0600

    Automatic Update

--- wallpapers/convert.sh
+++ wallpapers/convert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 convert wallpaper.png -resize 200x200 wpresize.png &&
 convert wpresize.png -gravity center -extent 100x100 wpresize.png &&
-convert -size 100x100 xc:none -fill wpresize.png -draw "circle 50,50, 20,0" usericon.png &&
+convert -size 100x100 xc:none -fill wpresize.png -draw "circle 50,50, 50,0" usericon.png &&
 mv usericon.png ~/dotfiles/config/lightdm/ &&
 rm wpresize.png
