fd73afea5314e929efb5453b29da87abdbcbf095
Dane Carder <skatardude10@gmail.com>
2016-06-20 00:43:48 -0600

    Automatic Update

--- scripts/rofi-volume.sh
+++ scripts/rofi-volume.sh
@@ -1,3 +1,3 @@
-vol=$(pactl list sinks | grep '^[[:space:]]Volume:' | \
-    head -n $(( $SINK + 3 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')
-var="$(echo -e "100\n90\n80\n70\n60\n50\n40\n30\n20\n10\n0" | rofi -dmenu -hide-scrollbar -separator-style "none" -mesg "Current: $vol %" -p "Set vol: ")"
+vol=$(amixer -D pulse sget Master | tail -n +7 | cut -f1 -d"]" | sed 's|.*\[\(.*\)|\1|'); 
+var="$(echo -e "100%\n90%\n80%\n70%\n60%\n50%\n40%\n30%\n20%\n10%\n0%" | rofi -dmenu -hide-scrollbar -width -13 -mesg "Cur vol: $vol" -p "Set: ")" &&
+amixer -D pulse sset Master $var;
---
+++ scripts/sys-stats.sh
@@ -0,0 +1,3 @@
+conky -c ~/dotfiles/rc/.conkyrc > ~/dotfiles/scripts/tmp &&
+mapfile -t sysinfos <~/dotfiles/scripts/tmp &&
+$(echo -e ""${sysinfos[0]}"\n"${sysinfos[4]}"\n"${sysinfos[1]}"\n"${sysinfos[2]}"\n"${sysinfos[3]}"" | rofi -dmenu -hide-scrollbar -width -20 -p "System Status: ")
