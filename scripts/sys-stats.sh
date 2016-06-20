73136e3df0e14e02fba9dc37cc2c11acf0e2d7ba
Dane Carder <skatardude10@gmail.com>
2016-06-20 16:39:22 -0600

    Automatic Update

--- scripts/sys-stats.sh
+++ scripts/sys-stats.sh
@@ -1,4 +1,4 @@
 conky -c ~/dotfiles/rc/.conkyrc > ~/dotfiles/scripts/tmp &&
 mapfile -t sysinfos <~/dotfiles/scripts/tmp &&
-$(echo -e ""${sysinfos[0]}"\n"${sysinfos[4]}"\n"${sysinfos[1]}"\n"${sysinfos[2]}"\n"${sysinfos[3]}"\n"${sysinfos[5]}"" | rofi -dmenu -hide-scrollbar -width -20 -p "System Status: ")
+$(echo -e ""${sysinfos[0]}"\n"${sysinfos[4]}"\n"${sysinfos[1]}"\n"${sysinfos[2]}"\n"${sysinfos[3]}"\n"${sysinfos[5]}"" | rofi -dmenu -hide-scrollbar -width -28 -p "System Status: ")
 rm ~/dotfiles/scripts/tmp
