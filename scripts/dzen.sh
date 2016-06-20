ae48a960de650d3aacebc6e1e35c09c17a43f817
Dane Carder <skatardude10@gmail.com>
2016-06-19 19:03:10 -0600

    Automatic Update

--- scripts/dzen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-UPDATE=1
-
-while :; do
-    DATE=`date +"%a %b %d %T %Z %Y"`
-    sleep $UPDATE
-done | dzen2 -fn '-*-liberation mono-medium-r-*-*-11-*-*-*-*-*-*-*' -bg '#2d2d2d' -fg '#000000' -ta r -p -y -1 -x -480 -title-name 'dzen right'
