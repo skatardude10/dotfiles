1622f08c6a519757a37aaf3d1b7c0fa34883d315
Dane Carder <skatardude10@gmail.com>
2016-06-20 18:02:47 -0600

    Automatic Update

--- scripts/sys-stats.sh
+++ scripts/sys-stats.sh
@@ -1,4 +1,9 @@
 conky -c ~/dotfiles/rc/.conkyrc > ~/dotfiles/scripts/tmp &&
 mapfile -t sysinfos <~/dotfiles/scripts/tmp &&
-$(echo -e ""${sysinfos[0]}"\n"${sysinfos[4]}"\n"${sysinfos[1]}"\n"${sysinfos[2]}"\n"${sysinfos[3]}"\n"${sysinfos[5]}"\n"${sysinfos[6]}"\n"${sysinfos[7]}"\n"${sysinfos[8]}"" | rofi -dmenu -hide-scrollbar -width -28 -p "System Status: ")
+var="$(echo -e ""${sysinfos[0]}"\n"${sysinfos[4]}"\n"${sysinfos[1]}"\n"${sysinfos[2]}"\n"${sysinfos[3]}"\n"${sysinfos[5]}"\n"${sysinfos[6]}"\n"${sysinfos[7]}"\n"${sysinfos[8]}"\nreset yabar" | rofi -dmenu -hide-scrollbar -width -28 -p "System Status: ")"
 rm ~/dotfiles/scripts/tmp
+if [ "$var" = "reset yabar" ] 
+then
+	pkill yabar
+	yabar -c ~/dotfiles/config/yabar.conf & disown
+fi
--- scripts/update.sh
+++ scripts/update.sh
@@ -3,5 +3,5 @@ while :
 do
 	checkupdates | wc -l > ~/.updates
 	curl -s http://rss.accuweather.com/rss/liveweather_rss.asp\?metric\=0\&locCode\=80016 | perl -ne 'if (/Currently/) {chomp;/\<title\>Currently: (.*)?\<\/title\>/; print "$1"; }' | tr -d ':' |  sed 's/$/   /' > ~/.weather
-	sleep 1200
+	sleep 300
 done
