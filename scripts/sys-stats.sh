ccba1bf1b9d413515ad3de4da4efc941fdf80dac
Dane Carder <skatardude10@gmail.com>
2016-06-20 19:50:51 -0600

    Automatic Update

--- scripts/sys-stats.sh
+++ scripts/sys-stats.sh
@@ -1,6 +1,6 @@
 conky -c ~/dotfiles/rc/.conkyrc > ~/dotfiles/scripts/tmp &&
 mapfile -t sysinfos <~/dotfiles/scripts/tmp &&
-var="$(echo -e ""${sysinfos[0]}"\n"${sysinfos[4]}"\n"${sysinfos[1]}"\n"${sysinfos[2]}"\n"${sysinfos[3]}"\n"${sysinfos[5]}"\n"${sysinfos[6]}"\n"${sysinfos[7]}"\n"${sysinfos[8]}"\nreset yabar" | rofi -dmenu -hide-scrollbar -width -28 -p "System Status: ")"
+var="$(echo -e "Hostname: $HOSTNAME\n"${sysinfos[0]}"\n"${sysinfos[4]}"\n"${sysinfos[1]}"\n"${sysinfos[2]}"\n"${sysinfos[3]}"\n"${sysinfos[5]}"\n"${sysinfos[6]}"\n"${sysinfos[7]}"\n"${sysinfos[8]}"\nreset yabar" | rofi -dmenu -hide-scrollbar -width -30 -p "System Status: ")"
 rm ~/dotfiles/scripts/tmp
 if [ "$var" = "reset yabar" ] 
 then
--- scripts/wallpaper-script.sh
+++ scripts/wallpaper-script.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 wallpaper=~/dotfiles/wallpapers/wallpaper.png
-wpout=~/wallpaper.png
-text=$(echo $HOSTNAME)
-convert -size 1920x1080 xc:none -gravity south -stroke black -strokewidth 2 -annotate 0 $text -background none -shadow 100x3+0+0 +repage -stroke none -fill white -annotate 0 $text $wallpaper  +swap -gravity south -geometry -20+1 -composite  $wpout
-feh --bg-fill $wpout
+feh --bg-fill $wallpaper
