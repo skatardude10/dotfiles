c8d4621bd63fef146fc6220576fa68388cf115f5
Dane Carder <skatardude10@gmail.com>
2016-08-06 16:38:51 -0600

    Automatic Update

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,5 +1,3 @@
-rsync -aAXHv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} / /path/to/backup/folder
-
 #!/bin/bash
 
 ## my own rsync-based snapshot-style backup procedure
@@ -7,22 +5,28 @@ rsync -aAXHv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","
 
 # config vars
 
-SRC="/home/username/files/" #dont forget trailing slash!
-SNAP="/snapshots/username"
-OPTS="-rltgoi --delay-updates --delete --chmod=a-w"
-MINCHANGES=20
+SRC="/" #dont forget trailing slash!
+SNAP="/mnt/Storage-Backup/System-Backup-Snapshots"
+OPTS="-rltgAoi --delay-updates --delete --chmod=a-w"
+MINCHANGES=10
 
 # run this process with real low priority
 
 ionice -c 3 -p $$
 renice +12  -p $$
 
+notify-send "Starting System Backup from $SRC to $SNAP"
+
 # sync
 
-rsync $OPTS $SRC $SNAP/latest >> $SNAP/rsync.log
+rsync $OPTS --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/Storage-Backup/*","/media/*","/lost+found"} $SRC $SNAP/latest >> $SNAP/rsync.log
+
+notify-send "Backup Sync Complete!"
 
 # check if enough has changed and if so
 # make a hardlinked copy named as the date
+
+notify-send "Finishing up backup..."
 
 COUNT=$( wc -l $SNAP/rsync.log|cut -d" " -f1 )
 if [ $COUNT -gt $MINCHANGES ] ; then
@@ -34,3 +38,5 @@ if [ $COUNT -gt $MINCHANGES ] ; then
                chmod u-w $SNAP/$DATETAG
          fi
 fi
+
+notify-send "Complete! $COUNT New Items in latest. See $SNAP/rsync.log"
