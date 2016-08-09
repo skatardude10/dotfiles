24416aff1a01eee0cd77d3d51d27973ddba09f90
Dane Carder <skatardude10@gmail.com>
2016-08-09 16:28:40 -0600

    Automatic Update

--- scripts/lightdm-greeter-apply.sh
+++ scripts/lightdm-greeter-apply.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo cp ~/dotfiles/config/lightdm/usericon.png /usr/share/pixmaps/usericon.png; sudo cp ~/dotfiles/wallpapers/wallpaper.png /usr/share/pixmaps/wallpaper.png; sudo cp ~/dotfiles/config/lightdm/lightdm.conf /etc/lightdm/lightdm.conf; sudo cp ~/dotfiles/config/lightdm/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
+sudo cp /home/carder/dotfiles/config/lightdm/usericon.png /usr/share/pixmaps/usericon.png; sudo cp /home/carder/dotfiles/wallpapers/wallpaper.png /usr/share/pixmaps/wallpaper.png; sudo cp /home/carder/dotfiles/config/lightdm/lightdm.conf /etc/lightdm/lightdm.conf; sudo cp /home/carder/dotfiles/config/lightdm/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
