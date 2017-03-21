daed779edc7844c2bbdf576ef6ed828f45bf28cc
Dane Carder <skatardude10@gmail.com>
2017-03-20 23:36:30 -0600

    Automatic Update

--- scripts/drop-term.sh
+++ scripts/drop-term.sh
@@ -3,7 +3,12 @@ sleep 1
 while true; do
 	if ps aux | grep '[u]rxvt -name dropterm' 1>&1
 		then
-			sleep 15
+			if ps aux | grep '[u]rxvt -name dropterm2' 1>&1
+				then
+					sleep 15
+				else
+					urxvt -name dropterm2 -geometry 175x30 -bg rgb:11/11/33
+			fi
 		else
 			urxvt -name dropterm -geometry 115x20 -bg rgb:11/11/33
 	fi
