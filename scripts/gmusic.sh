4697c26cd1aee0f7d1454286d3e9f3482150b7f5
Dane Carder <skatardude10@gmail.com>
2017-03-28 23:07:38 -0600

    Automatic Update

--- scripts/gmusic.sh
+++ scripts/gmusic.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
-#     _____ _         _                _           _     __  ___  _     
-#    / ____| |       | |              | |         | |   /_ |/ _ \( )    
-#   | (___ | | ____ _| |_ __ _ _ __ __| |_   _  __| | ___| | | | |/ ___ 
-#    \___ \| |/ / _` | __/ _` | '__/ _` | | | |/ _` |/ _ \ | | | | / __|
-#    ____) |   < (_| | || (_| | | | (_| | |_| | (_| |  __/ | |_| | \__ \
-#   |_____/|_|\_\__,_|\__\__,_|_|  \__,_|\__,_|\__,_|\___|_|\___/  |___/
-#     _____                   _        __  __           _               
-#    / ____|                 | |      |  \/  |         (_)              
-#   | |  __  ___   ___   __ _| | ___  | \  / |_   _ ___ _  ___          
-#   | | |_ |/ _ \ / _ \ / _` | |/ _ \ | |\/| | | | / __| |/ __|         
-#   | |__| | (_) | (_) | (_| | |  __/ | |  | | |_| \__ \ | (__          
-#    \_____|\___/ \___/ \__, |_|\___| |_|  |_|\__,_|___/_|\___|         
-#                        __/ |                                          
-#    _    _      _      |___/____           _       _                   
-#   | |  | |    | |        / ____|         (_)     | |                  
-#   | |__| | ___| |_ __   | (___   ___ _ __ _ _ __ | |_                 
-#   |  __  |/ _ \ | '_ \   \___ \ / __| '__| | '_ \| __|                
-#   | |  | |  __/ | |_) |  ____) | (__| |  | | |_) | |_                 
-#   |_|  |_|\___|_| .__/  |_____/ \___|_|  |_| .__/ \__|                
-#                 | |                        | |                        
-#                 |_|                        |_|                        
-
-
+#  ╔═╗┬┌─┌─┐┌┬┐┌─┐┬─┐┌┬┐┬ ┬┌┬┐┌─┐┌─┐      
+#  ╚═╗├┴┐├─┤ │ ├─┤├┬┘ │││ │ ││├┤ └─┐      
+#  ╚═╝┴ ┴┴ ┴ ┴ ┴ ┴┴└──┴┘└─┘─┴┘└─┘└─┘      
+#  ╔═╗┌─┐┌─┐┌─┐┬  ┌─┐  ╔╦╗┬ ┬┌─┐┬┌─┐      
+#  ║ ╦│ ││ ││ ┬│  ├┤   ║║║│ │└─┐││        
+#  ╚═╝└─┘└─┘└─┘┴─┘└─┘  ╩ ╩└─┘└─┘┴└─┘      
+#  ╔═╗┬  ┌─┐┬ ┬┬  ┬┌─┐┌┬┐  ╔╦╗┌─┐┬┌─┌─┐┬─┐
+#  ╠═╝│  ├─┤└┬┘│  │└─┐ │   ║║║├─┤├┴┐├┤ ├┬┘
+#  ╩  ┴─┘┴ ┴ ┴ ┴─┘┴└─┘ ┴   ╩ ╩┴ ┴┴ ┴└─┘┴└─
 gmusic=$(pgrep GMusicProxy)
 if [ $gmusic ]
 then
@@ -30,8 +16,6 @@ then
 	echo "  |   __| |_ ___| |_ ___ ___ _| |_ _ _| |___|_  | |   |___   "
 	echo "  |__   | '_| .'|  _| .'|  _| . | | | . | -_|_| |_| | |_ -|  "
 	echo "  |_____|_,_|__,|_| |__,|_| |___|___|___|___|_____|___|___|  "
-	echo "                                                             "
-	echo "                                                             "
 	echo "   _____               _        _____     _                  "
 	echo "  |   __|_____ _ _ ___|_|___   |  |  |___| |___ ___ ___      "
 	echo "  |  |  |     | | |_ -| |  _|  |     | -_| | . | -_|  _|     "
@@ -39,11 +23,16 @@ then
 	echo "   Press Enter to continue:                |_|           "
 	read
 	clear
-	echo "GMusicProxy is Running :-)"
+	echo "╔═╗┬  ┌─┐┬ ┬┬  ┬┌─┐┌┬┐"
+	echo "╠═╝│  ├─┤└┬┘│  │└─┐ │ "
+	echo "╩  ┴─┘┴ ┴ ┴ ┴─┘┴└─┘ ┴ "
 	echo "[n]ew playlist  -or-  [a]dd to Now Playing?"
 	echo " "	
 	read AddorDel
 	clear
+	echo "╔═╗┬  ┌─┐┬ ┬┬  ┬┌─┐┌┬┐"
+	echo "╠═╝│  ├─┤└┬┘│  │└─┐ │ "
+	echo "╩  ┴─┘┴ ┴ ┴ ┴─┘┴└─┘ ┴ "
 	echo "Create playlist by Similar:"
 	echo ""
 	echo "[a] Artist"
@@ -55,69 +44,147 @@ then
 	clear
 	case $Search in
 		a)
+			echo "╔═╗┬─┐┌┬┐┬┌─┐┌┬┐"
+			echo "╠═╣├┬┘ │ │└─┐ │ "
+			echo "╩ ╩┴└─ ┴ ┴└─┘ ┴ "
 			echo "Enter Artist:"
 			read Artist
 			clear
+			echo "╔╗╔┬ ┬┌┬┐┌┐ ┌─┐┬─┐"
+			echo "║║║│ ││││├┴┐├┤ ├┬┘"
+			echo "╝╚╝└─┘┴ ┴└─┘└─┘┴└─"
 			echo "How many songs to pull?"
 			read Number
 			clear
 				case $AddorDel in
 					n)
+						echo "╦ ╦┌─┐┬─┐┬┌─┬┌┐┌┌─┐   "
+						echo "║║║│ │├┬┘├┴┐│││││ ┬   "
+						echo "╚╩╝└─┘┴└─┴ ┴┴┘└┘└─┘ooo"
+						echo "Getting your playlist based off $Artist..."
 						curl -s $(echo "http://localhost:9999/get_new_station_by_search?type=artist&artist=$Artist&num_tracks=$Number" | tr ' ' '%20')  > $(echo "$HOME/.config/mpd/playlists/$Artist.m3u" | tr ' ' '-')
+						clear
 						cat $(echo "$HOME/.config/mpd/playlists/$Artist.m3u" | tr ' ' '-') | grep -v '^http' | cut -d, -f2-
+						echo "╔╦╗┌─┐┌┐┌┌─┐┬"
+						echo " ║║│ ││││├┤ │"
+						echo "═╩╝└─┘┘└┘└─┘o"
 						echo "Created new playlist with $Number songs similar to songs by $Artist!"
+						echo " "
 						;;
 					a)
+						echo "╦ ╦┌─┐┬─┐┬┌─┬┌┐┌┌─┐   "
+						echo "║║║│ │├┬┘├┴┐│││││ ┬   "
+						echo "╚╩╝└─┘┴└─┴ ┴┴┘└┘└─┘ooo"
+						echo "Adding songs to Now Playing based off $Artist..."
 						curl -s $(echo "http://localhost:9999/get_new_station_by_search?type=artist&artist=$Artist&num_tracks=$Number" | tr ' ' '%20') | grep -v ^# | while read url; do mpc add "$url"; done
+						clear
+						echo "╔╦╗┌─┐┌┐┌┌─┐┬"
+						echo " ║║│ ││││├┤ │"
+						echo "═╩╝└─┘┘└┘└─┘o"
 						echo "Added $Number songs similar to $Artist to Now Playing!"
+						echo " "
 						;;
 				esac
 			;;
 		b)
+			echo "╔═╗┬  ┌┐ ┬ ┬┌┬┐"
+			echo "╠═╣│  ├┴┐│ ││││"
+			echo "╩ ╩┴─┘└─┘└─┘┴ ┴"
 			echo "Enter Album:"
 			read Album
 			clear
+			echo "╔╗╔┬ ┬┌┬┐┌┐ ┌─┐┬─┐"
+			echo "║║║│ ││││├┴┐├┤ ├┬┘"
+			echo "╝╚╝└─┘┴ ┴└─┘└─┘┴└─"
 			echo "How many songs to pull?"
 			read Number
 			clear
 				case $AddorDel in
 					n)
+						echo "╦ ╦┌─┐┬─┐┬┌─┬┌┐┌┌─┐   "
+						echo "║║║│ │├┬┘├┴┐│││││ ┬   "
+						echo "╚╩╝└─┘┴└─┴ ┴┴┘└┘└─┘ooo"
+						echo "Getting your playlist based off $Album..."
 						curl -s $(echo "http://localhost:9999/get_new_station_by_search?type=album&title=$Album&num_tracks=$Number" | tr ' ' '%20') > $(echo "$HOME/.config/mpd/playlists/$Album.m3u" | tr ' ' '-')
+						clear
 						cat $(echo "$HOME/.config/mpd/playlists/$Album.m3u" | tr ' ' '-') | grep -v '^http' | cut -d, -f2-
+						echo "╔╦╗┌─┐┌┐┌┌─┐┬"
+						echo " ║║│ ││││├┤ │"
+						echo "═╩╝└─┘┘└┘└─┘o"
 						echo "Created new playlist with $Number songs similar to songs in $Album!"
+						echo " "
 						;;
 					a)
+						echo "╦ ╦┌─┐┬─┐┬┌─┬┌┐┌┌─┐   "
+						echo "║║║│ │├┬┘├┴┐│││││ ┬   "
+						echo "╚╩╝└─┘┴└─┴ ┴┴┘└┘└─┘ooo"
+						echo "Adding songs to Now Playing based off $Album..."
 						curl -s $(echo "http://localhost:9999/get_new_station_by_search?type=album&title=$Album&num_tracks=$Number" | tr ' ' '%20') | grep -v ^# | while read url; do mpc add "$url"; done
+						clear
+						echo "╔╦╗┌─┐┌┐┌┌─┐┬"
+						echo " ║║│ ││││├┤ │"
+						echo "═╩╝└─┘┘└┘└─┘o"
 						echo "Added $Number songs similar to songs from $Album to Now Playing!"
+						echo " "
 						;;
 				esac
 			;;
 		s)
+			echo "╔═╗┌─┐┌┐┌┌─┐"
+			echo "╚═╗│ │││││ ┬"
+			echo "╚═╝└─┘┘└┘└─┘"
 			echo "Enter Song:"
 			read Song
 			clear
+			echo "╔╗╔┬ ┬┌┬┐┌┐ ┌─┐┬─┐"
+			echo "║║║│ ││││├┴┐├┤ ├┬┘"
+			echo "╝╚╝└─┘┴ ┴└─┘└─┘┴└─"
 			echo "How many songs to pull?"
 			read Number
 			clear
 				case $AddorDel in
 					n)
+						echo "╦ ╦┌─┐┬─┐┬┌─┬┌┐┌┌─┐   "
+						echo "║║║│ │├┬┘├┴┐│││││ ┬   "
+						echo "╚╩╝└─┘┴└─┴ ┴┴┘└┘└─┘ooo"
+						echo "Getting your playlist based off $Song..."
 						curl -s $(echo "http://localhost:9999/get_new_station_by_search?type=song&title=$Song&num_tracks=$Number" | tr ' ' '%20') > $(echo "$HOME/.config/mpd/playlists/$Song.m3u" | tr ' ' '-')
+						clear
 						cat $(echo "$HOME/.config/mpd/playlists/$Song.m3u" | tr ' ' '-') | grep -v '^http' | cut -d, -f2-
+						echo "╔╦╗┌─┐┌┐┌┌─┐┬"
+						echo " ║║│ ││││├┤ │"
+						echo "═╩╝└─┘┘└┘└─┘o"
 						echo "Created new playlist with $Number songs similar to $Song!"
+						echo " "
 						;;
 					a)
+						echo "╦ ╦┌─┐┬─┐┬┌─┬┌┐┌┌─┐   "
+						echo "║║║│ │├┬┘├┴┐│││││ ┬   "
+						echo "╚╩╝└─┘┴└─┴ ┴┴┘└┘└─┘ooo"
+						echo "Adding songs to Now Playing based off $Song..."
 						curl -s $(echo "http://localhost:9999/get_new_station_by_search?type=song&title=$Song&num_tracks=$Number" | tr ' ' '%20') | grep -v ^# | while read url; do mpc add "$url"; done
+						clear
+						echo "╔╦╗┌─┐┌┐┌┌─┐┬"
+						echo " ║║│ ││││├┤ │"
+						echo "═╩╝└─┘┘└┘└─┘o"
 						echo "Added $Number songs similar to $Song to Now Playing!"
+						echo " "
 						;;
 				esac
 			;;
 		l)
+			echo "╔╗╔┬ ┬┌┬┐┌┐ ┌─┐┬─┐"
+			echo "║║║│ ││││├┴┐├┤ ├┬┘"
+			echo "╝╚╝└─┘┴ ┴└─┘└─┘┴└─"
 			echo "How many songs to pull per liked song?"
 			read Number
 			clear
 			LikeFile=$HOME/dotfiles/songs/like
 			TmpLike=$HOME/dotfiles/songs/tmplike
 			Date=`date +%y-%m-%d-%H-%M-%S`
+			echo "╦ ╦┌─┐┬─┐┬┌─┬┌┐┌┌─┐   "
+			echo "║║║│ │├┬┘├┴┐│││││ ┬   "
+			echo "╚╩╝└─┘┴└─┴ ┴┴┘└┘└─┘ooo"
 			echo "Creating Like-Playlist-$Date.m3u "
 			sleep 2
 			clear
@@ -127,12 +194,18 @@ then
 						NumInLike=$(cat $TmpLike | wc -l)
 						Countfile=$HOME/dotfiles/songs/countfile
 						echo 0 > $Countfile
+						echo "╦ ╦┌─┐┬─┐┬┌─┬┌┐┌┌─┐   "
+						echo "║║║│ │├┬┘├┴┐│││││ ┬   "
+						echo "╚╩╝└─┘┴└─┴ ┴┴┘└┘└─┘ooo"
 						echo "$(cat $Countfile) of $NumInLike complete..."
 						while read songs; do
 							curl -s $(echo "http://localhost:9999/get_new_station_by_id?id=$songs&type=song&num_tracks=$Number") >> $(echo "$HOME/.config/mpd/playlists/Like-Playlist-$Date.m3u" | tr ' ' '-')
 							Count=$[$(cat $Countfile) + 1]
 							echo $Count > $Countfile
 							clear
+							echo "╦ ╦┌─┐┬─┐┬┌─┬┌┐┌┌─┐   "
+							echo "║║║│ │├┬┘├┴┐│││││ ┬   "
+							echo "╚╩╝└─┘┴└─┴ ┴┴┘└┘└─┘ooo"
 							echo "$(cat $Countfile) of $NumInLike complete..."
 						done <$TmpLike
 						clear
@@ -140,6 +213,9 @@ then
 						sed -i '1s/^/#EXTM3U\n/' $HOME/.config/mpd/playlists/Like-Playlist-$Date.m3u
 						cat $(echo "$HOME/.config/mpd/playlists/Like-Playlist-$Date.m3u") | grep -v '^http' | cut -d, -f2-
 						echo " "
+						echo "╔╦╗┌─┐┌┐┌┌─┐┬"
+						echo " ║║│ ││││├┤ │"
+						echo "═╩╝└─┘┘└┘└─┘o"
 						echo "Created new playlist from $NumInLike liked songs with $Number songs per liked song, for a total number of $(grep -c "http" $HOME/.config/mpd/playlists/Like-Playlist-$Date.m3u) songs in your new playlist!"
 						echo " "
 						rm $TmpLike
@@ -150,16 +226,25 @@ then
 						NumInLike=$(cat $TmpLike | wc -l)
 						Countfile=$HOME/dotfiles/songs/countfile
 						echo 0 > $Countfile
+						echo "╦ ╦┌─┐┬─┐┬┌─┬┌┐┌┌─┐   "
+						echo "║║║│ │├┬┘├┴┐│││││ ┬   "
+						echo "╚╩╝└─┘┴└─┴ ┴┴┘└┘└─┘ooo"
 						echo "$(cat $Countfile) of $NumInLike complete..."
 						while read songs; do
 							curl -s $(echo "http://localhost:9999/get_new_station_by_id?id=$songs&type=song&num_tracks=$Number") | grep -v ^# | while read url; do mpc add "$url"; done
 							Count=$[$(cat $Countfile) + 1]
 							echo $Count > $Countfile
 							clear
+							echo "╦ ╦┌─┐┬─┐┬┌─┬┌┐┌┌─┐   "
+							echo "║║║│ │├┬┘├┴┐│││││ ┬   "
+							echo "╚╩╝└─┘┴└─┴ ┴┴┘└┘└─┘ooo"
 							echo "$(cat $Countfile) of $NumInLike complete..."
 						done <$TmpLike
 						clear
 						echo " "
+						echo "╔╦╗┌─┐┌┐┌┌─┐┬"
+						echo " ║║│ ││││├┤ │"
+						echo "═╩╝└─┘┘└┘└─┘o"
 						echo "Added $(($NumInLike * $Number)) songs to Now Playing!"
 						echo " "
 						rm $TmpLike
@@ -169,5 +254,9 @@ then
 			;;
 	esac
 else
+	clear
+	echo "╔═╗╦═╗╦═╗╔═╗╦═╗┬┬┬"
+	echo "║╣ ╠╦╝╠╦╝║ ║╠╦╝│││"
+	echo "╚═╝╩╚═╩╚═╚═╝╩╚═ooo"
 	echo "GMusicProxy is not running! Please start GMusicProxy before using this..."
 fi
