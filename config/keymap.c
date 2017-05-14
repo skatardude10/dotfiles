01861961a028e28c362b35294526cbd04879fe29
Dane Carder <skatardude10@gmail.com>
2017-05-14 14:18:58 -0600

    Automatic Update

--- config/keymap.c
+++ config/keymap.c
@@ -11,6 +11,8 @@ extern rgblight_config_t rgblight_config;
 #define _RAISE 2
 #define _ADJUST 3
 #define _WINMAN 4
+#define _GAMING 5
+#define _GAMEXTRA 6
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -18,6 +20,8 @@ enum custom_keycodes {
   RAISE,
   ADJUST,
   WINMAN,
+  GAMING,
+  GAMEXTRA,
 };
 
 // increase readability 
@@ -95,16 +99,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, KC_LALT, KC_LGUI,     F(0),      F(1),     ADJUST, RALT(KC_GRAVE), KC_RCTL \
 		),
 
-/*
-  Below works with RGB code, while above ACTION_LAYER_TAP_KEYs F(0), F(1), F(2), F(3) when going to layers on hold do not trigger RGB code.
-    [_QWERTY] = KEYMAP( \
-        KC_ESC,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,   KC_BSPC,\
-        KC_TAB,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    WINMAN,\
-        KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_RSFT,\
-        KC_LCTL, KC_LALT, KC_LGUI,          LOWER,           RAISE,        ADJUST, RALT(KC_GRAVE), KC_RCTL \
-    ),
-*/
-
     [_LOWER] = KEYMAP( \
         KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, \
 		KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS, \
@@ -123,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         RGB_TOG, KC_WH_L, KC_MS_U, KC_WH_R, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_PAUS, \
 		KC_BTN3, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_U, XXXXXXX, XXXXXXX, KC_PGUP, KC_PGDOWN, XXXXXXX,  XXXXXXX,  \
 		KC_LGUI, XXXXXXX, XXXXXXX, KC_WH_D,  XXXXXXX, M_BRTOG, M_BSPDU, M_BSPDD, M_BDFLT, BL_TOGG, BL_INC, \
-		_______, _______, _______, KC_BTN1, KC_BTN2, _______, BL_TOGG, BL_DEC \
+		TG(_GAMING), _______, _______, KC_BTN1, KC_BTN2, _______, BL_TOGG, BL_DEC \
 		),
 
     [_WINMAN] = KEYMAP( \
@@ -131,6 +125,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, M_WINL, M_WIND, M_WINR, M_FULS, XXXXXXX, M_HORZ, M_MOVL, M_MOVD, M_MOVR, _______, \
     _______, XXXXXXX, XXXXXXX, M_NET, M_VERT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, \
     _______, _______, M_MWSL, M_GWSL, M_GWSR, M_MWSR, _______, _______ \
+    ),
+
+    [_GAMING] = KEYMAP( \
+    KC_ESC,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,   KC_BSPC,\
+    KC_TAB,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_ENT,\
+    KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_RSFT,\
+    KC_LCTL, KC_LALT, KC_LGUI,     KC_SPC,      GAMEXTRA,     TO(_QWERTY), RALT(KC_GRAVE), KC_RCTL \
+    ),
+
+    [_GAMEXTRA] = KEYMAP( \
+    KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, \
+    KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS, \
+    _______, _______, _______, _______, _______, KC_QUOTE, KC_SLASH, KC_LBRACKET, KC_RBRACKET, KC_BSLASH, KC_EQUAL, \
+    _______, _______, _______, _______, _______, _______, _______, _______ \
     ),
 
 };
@@ -150,6 +158,7 @@ const uint16_t PROGMEM fn_actions[] = {
   [0]  = ACTION_LAYER_TAP_KEY(_LOWER, KC_SPC),
   [1]  = ACTION_LAYER_TAP_KEY(_RAISE, KC_SPC),
   [2]  = ACTION_LAYER_TAP_KEY(_WINMAN, KC_ENT),
+  [3]  = ACTION_LAYER_TAP_KEY(_GAMEXTRA, KC_SPC),
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
@@ -356,6 +365,9 @@ void matrix_scan_user(void) {
           rgblight_mode(29);
         }
         break;
+    case _GAMING:
+        rgblight_mode(3);
+        break;
     case _QWERTY:
         rgblight_mode(RGB_current_mode);   // revert RGB to initial mode prior to RGB mode change
         TOG_STATUS = false;
@@ -382,6 +394,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         rgblight_mode(RGB_current_mode);   // revert RGB to initial mode prior to RGB mode change
         TOG_STATUS = false;
         layer_off(_ADJUST);
+      }
+      return false;
+      break;
+    case GAMEXTRA:
+      if (record->event.pressed) {
+        layer_on(_GAMEXTRA);
+      } else {
+        layer_off(_GAMEXTRA);
       }
       return false;
       break;
