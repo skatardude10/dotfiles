e7ab16aecfe3647f7a971e8581796221ec5d9ebc
Dane Carder <skatardude10@gmail.com>
2017-06-25 12:09:23 -0600

    Automatic Update

--- config/keymap.c
+++ config/keymap.c
@@ -96,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,   KC_BSPC,\
         KC_TAB,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    F(2),\
         KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_RSFT,\
-        KC_LCTL, KC_LALT, KC_LGUI,     F(0),      F(1),     ADJUST, RALT(KC_GRAVE), KC_RCTL \
+        KC_LCTL, KC_LGUI, KC_LALT,     F(0),      F(1),     ADJUST, RALT(KC_GRAVE), KC_RCTL \
 		),
 
     [_LOWER] = KEYMAP( \
@@ -131,7 +131,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,   KC_BSPC,\
     KC_TAB,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_ENT,\
     KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_RSFT,\
-    KC_LCTL, KC_LALT, KC_LGUI,     KC_SPC,      GAMEXTRA,     TO(_QWERTY), RALT(KC_GRAVE), KC_RCTL \
+    KC_LCTL, KC_LGUI, KC_LALT,     KC_SPC,      GAMEXTRA,     TO(_QWERTY), RALT(KC_GRAVE), KC_RCTL \
     ),
 
     [_GAMEXTRA] = KEYMAP( \
