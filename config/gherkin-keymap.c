671d0aa0daa96957429c27d3c89f064f4fb339e9
Dane Carder <skatardude10@gmail.com>
2017-06-27 08:27:05 -0600

    Automatic Update

--- config/gherkin-keymap.c
+++ config/gherkin-keymap.c
@@ -76,7 +76,8 @@ enum custom_keycodes {
 enum {
   TD_Q_ESC = 0,
   TD_SFT_CTL = 1,
-  TD_A_TAB = 2
+  TD_A_TAB = 2, 
+  TD_5_DOT = 3
 };
 
 //Tap Dance Definitions
@@ -84,45 +85,46 @@ qk_tap_dance_action_t tap_dance_actions[] = {
   //Tap once for Esc, twice for Caps Lock
   [TD_Q_ESC]  = ACTION_TAP_DANCE_DOUBLE(KC_Q, KC_ESC),
   [TD_SFT_CTL]  = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_LCTL),
-  [TD_A_TAB]  = ACTION_TAP_DANCE_DOUBLE(KC_A, KC_TAB)
+  [TD_A_TAB]  = ACTION_TAP_DANCE_DOUBLE(KC_A, KC_TAB), 
+  [TD_5_DOT]  = ACTION_TAP_DANCE_DOUBLE(KC_P5, KC_PDOT)
 // Other declarations would go here, separated by commas, if you have them
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = KEYMAP(
     TD(0),    KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,  
-    TD(2),    XXXXXXX,  KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     F(4),
-    CTL_T(KC_Z), GUI_T(KC_X), ALT_T(KC_C), F(0), F(1), F(2),    F(3),     KC_M,     KC_S,    TD(1))
+    TD(2),    KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     XXXXXXX,     F(4),
+    CTL_T(KC_Z), GUI_T(KC_X), ALT_T(KC_C), F(0), F(1), F(2),    F(3),     KC_M,     KC_L,    TD(1))
   ,
   [_ZERO] = KEYMAP(
     KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,
     KC_RSFT,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,
-    _______,   _______,  _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_COMM,  KC_DOT,  _______)
+    _______,  _______,  _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_COMM,  KC_DOT,   _______)
   ,
   [_ONE] = KEYMAP(
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_DEL,  KC_BSPC,
-    KC_RSFT,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_QUOT,  KC_GRV,   KC_SCLN,  KC_MINS,  KC_EQL,
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_EQL,   KC_DEL,   KC_BSPC,
+    KC_RSFT,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_QUOT,  KC_GRV,   KC_SCLN,  XXXXXXX,  KC_MINS,
     _______,  _______,  _______,  XXXXXXX,  _______,  KC_SLSH,  KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_RSFT)
   ,
   [_TWO] = KEYMAP(
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_PGDOWN,  KC_UP,  KC_PGUP,  KC_BSPC,
-    KC_CAPS,  XXXXXXX,  _______,  XXXXXXX,  XXXXXXX,  KC_HOME,  KC_LEFT,  KC_DOWN,  KC_RIGHT,  KC_PSCREEN,
-    _______,  _______,  _______,  XXXXXXX,  RALT(KC_GRAVE),  _______,  KC_END,  XXXXXXX,  XXXXXXX,  _______)
+    KC_CAPS,  KC_PGDOWN, KC_UP,   KC_PGUP,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_DEL,   KC_BSPC,
+    _______,  KC_LEFT,  KC_DOWN,  KC_RIGHT,  KC_END,  KC_HOME,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_PSCREEN,
+    _______,  _______,  _______,  KC_HOME, RALT(KC_GRAVE), _______, KC_END, XXXXXXX, XXXXXXX, _______)
   ,
   [_ADJUST] = KEYMAP(
-    RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  XXXXXXX,  KC_WH_D,  KC_MS_U,  KC_WH_U,  KC_PAUS,
-    XXXXXXX,  XXXXXXX,  RGB_VAD,  RGB_HUD,  RGB_SAD,  XXXXXXX,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_BTN3,
-    TG(_TOGGLE),   _______,  _______,  XXXXXXX,  KC_BTN1,  KC_BTN2,  _______,  XXXXXXX,  XXXXXXX,  _______)
+    RGB_TOG,  KC_WH_D,  KC_MS_U,  KC_WH_U,  RGB_HUI,  RGB_SAI,  RGB_VAI,  BL_INC,   BL_TOGG,  KC_PAUS,
+    RGB_MOD,  KC_MS_L,  KC_MS_D,  KC_MS_R,  RGB_HUD,  RGB_SAD,  RGB_VAD,  BL_DEC,   XXXXXXX,  KC_BTN3,
+    _______,  _______,  _______,  XXXXXXX,  KC_BTN1,  KC_BTN2,  _______,  XXXXXXX,  XXXXXXX,  _______)
   ,
   [_WINMAN] = KEYMAP(
-    M_KILL,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  M_WINU,   M_GDEC,   M_GINC,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  M_FULS,   M_VERT,   M_HORZ,   M_WINL,   M_WIND,   M_WINR,   _______,
-    XXXXXXX,  XXXXXXX,  M_NET,    M_MWSL,   M_GWSL,   M_GWSR,   M_MWSR,   XXXXXXX,  XXXXXXX,  _______)
+    M_KILL,   XXXXXXX,  M_WINU,   M_FULS,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,   M_GDEC,   M_GINC,
+    KC_RSFT,  M_WINL,   M_WIND,   M_WINR,  M_VERT,   M_HORZ,   XXXXXXX,  XXXXXXX,   XXXXXXX,  _______,
+    XXXXXXX,  XXXXXXX,  M_NET,    M_MWSL,  M_GWSL,   M_GWSR,   M_MWSR,   XXXXXXX,   XXXXXXX,  TG(_TOGGLE))
   ,
   [_TOGGLE] = KEYMAP(
-    KC_NLCK,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_PAST,  KC_P7,  KC_P8,  KC_P9,  KC_PMNS,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_PSLS,  KC_P4,  KC_P5,  KC_P6,  KC_PPLS,
-    TO(_QWERTY),   XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_PDOT,  KC_P0,  KC_P1,  KC_P2,  KC_P3,  KC_PENT)
+    KC_PAST,  KC_P7,  KC_P8,  KC_P9,  KC_PMNS,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_NLCK,
+    KC_PSLS,  KC_P4,  TD(3),  KC_P6,  KC_PPLS,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    KC_P0,    KC_P1,  KC_P2,  KC_P3,  KC_PENT,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  TO(_QWERTY))
 }; 
 
 enum function_id {
@@ -283,7 +285,7 @@ void matrix_scan_user(void) {
         if (TOG_STATUS) { //TOG_STATUS checks is another reactive key currently pressed, only changes RGB mode if returns false
         } else {
           TOG_STATUS = !TOG_STATUS;
-          rgblight_mode(29);
+          rgblight_mode(4);
         }
         break;
     case _ONE:
@@ -294,7 +296,7 @@ void matrix_scan_user(void) {
         if (TOG_STATUS) { //TOG_STATUS checks is another reactive key currently pressed, only changes RGB mode if returns false
         } else {
           TOG_STATUS = !TOG_STATUS;
-          rgblight_mode(29);
+          rgblight_mode(5);
         }
         break;
     case _TWO:
@@ -305,7 +307,7 @@ void matrix_scan_user(void) {
         if (TOG_STATUS) { //TOG_STATUS checks is another reactive key currently pressed, only changes RGB mode if returns false
         } else {
           TOG_STATUS = !TOG_STATUS;
-          rgblight_mode(29);
+          rgblight_mode(26);
         }
         break;
     case _WINMAN:
@@ -327,11 +329,11 @@ void matrix_scan_user(void) {
         if (TOG_STATUS) { //TOG_STATUS checks is another reactive key currently pressed, only changes RGB mode if returns false
         } else {
           TOG_STATUS = !TOG_STATUS;
-          rgblight_mode(29);
+          rgblight_mode(25);
         }
         break;
     case _TOGGLE:
-        rgblight_mode(3);
+        rgblight_mode(20);
         break;
     case _QWERTY:
         if (RGB_INIT) {} else {
